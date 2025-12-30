fcefd9fcfe22b5b250dd589a047d9443f316bd5c
eRodrigoL <e.rodrigo.l@outlook.com>
2025-12-30 15:03:10 -0300

    feat: implementa `lista de tarefa` estática
    - ajusta estética do componente `toolar``
    - adiciona list de tarefas estática
      - usa `Grid List` e `Card` da biblioteca Angular Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,46 @@
+// src/app/app.ts
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+
+export interface Tarefa {
+  id: number;
+  dataLimite: string | Date;
+  resumo: string;
+  descricao: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatGridListModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-json-server-todo-list');
+  tarefas: Tarefa[] = [
+    {
+      id: 1,
+      dataLimite: '25/04/2026',
+      resumo: 'Consulta com Dra Ana',
+      descricao:
+        'Consulta médica com a Dra Ana (NECESSÁRIO levar resultado dos exames solicitados)',
+    },
+    {
+      id: 2,
+      dataLimite: '03/08/2026',
+      resumo: 'Renovar CNH',
+      descricao: 'Renovar CNH antes do vencimento 2 meses após',
+    },
+    {
+      id: 3,
+      dataLimite: '05/01/2026',
+      resumo: 'Pagar conta de luz',
+      descricao: 'Valor: R$ 268,41',
+    },
+  ];
 }
