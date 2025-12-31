f12453f3a183313ab42ffdb6d411dfae47871a06
eRodrigoL <e.rodrigo.l@outlook.com>
2025-12-31 19:33:38 -0300

    feat: adiciona componente CardModal

---
+++ src/app/components/card-modal/card-modal.ts
@@ -0,0 +1,24 @@
+// src/app/components/card-modal/card-modal.ts
+
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-card-modal',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+  ],
+  templateUrl: './card-modal.html',
+  styleUrl: './card-modal.scss',
+})
+export class CardModal {}
